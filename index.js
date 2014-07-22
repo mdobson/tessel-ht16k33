9d58a78d509d36b0294a95615d1c372e430f4b46
Matt Dobson <mdobson4@gmail.com>
2014-07-21 22:50:03 -0400

    Added index to show off some sample.

--- index.js
+++ index.js
@@ -1,34 +1,45 @@
 var tessel = require('tessel');
+var LEDBackpack = require('./backpack');
+
 var port = tessel.port.A;
 var slave = 0x70;
 
-var i2c = new port.I2C(slave);
-console.log('Begin transfer');
-i2c.send(new Buffer([0x21]), function(err) {
-  console.log('Initializing connection to backpack...');
-  console.log('turning on the oscillator...');
-  if(err) {
-    console.log('Error:', err);
-  }
-  i2c.send(new Buffer([0xEF]), function(err) {
+var bp = new LEDBackpack({addr: slave, port: port});
+
+bp.on('ready', function() {
+ 
+  setTimeout(function() {
+    bp.clear();
+  }, 2000);
+});
+
+function writeSmileyInBytes(bp) {
+  bp.write(new Buffer([0x00, 0x1E, 0x00, 0x21, 0x00, 0xD2, 0x00, 0xC0, 0x00, 0xD2, 0x00, 0xCC, 0x00, 0x21, 0x00, 0x1E, 0x00]), function(err) {
     if(err) {
       console.log('Error:', err);
     }
-    console.log('Setting brightness to max...');
-    i2c.send(new Buffer([0x81]), function(err) {
-      if(err) {
-        console.log('Error:', err);
-      }
-      console.log('Turn blink off and display on...');
-      setTimeout(function() {
-        i2c.send(new Buffer([0x00, 0x1E, 0x00, 0x21, 0x00, 0xD2, 0x00, 0xC0, 0x00, 0xD2, 0x00, 0xCC, 0x00, 0x21, 0x00, 0x1E, 0x00]), function(err) {
-          if(err) {
-            console.log('Error:', err);
-          }
-          console.log('writing data to animate LEDs...');
-        });
-      }, 2000);
-    });
+    console.log('writing data to animate LEDs...');
   });
-});
+}
+
+function writeSadInBitmap(bp) {
+  var bmp = [
+    [ 0,0,1,1,1,1,0,0 ],
+    [ 0,1,0,0,0,0,1,0 ],
+    [ 1,0,1,0,0,1,0,1 ],
+    [ 1,0,0,0,0,0,0,1 ],
+    [ 1,0,0,1,1,0,0,1 ],
+    [ 1,0,1,0,0,1,0,1 ],
+    [ 0,1,0,0,0,0,1,0 ],
+    [ 0,0,1,1,1,1,0,0 ]
+  ]
+
+  bp.writeBitmap(bmp, function(err) {
+    if(err) {
+      console.log('Error:', err);
+    }
+    console.log('Writing data to animate LEDs...');
+  });
+}
+
 
